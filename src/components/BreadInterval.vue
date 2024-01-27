03ce519b8f8d06d7bc4fd85cbeb9f17b688d6385
Cameron-Ord <cameron.ian.ronald.ord@outlook.com>
2024-01-27 11:12:25 -0700

    adjustments/animations

--- src/components/BreadInterval.vue
+++ src/components/BreadInterval.vue
@@ -1,24 +1,25 @@
 
 <style lang="scss" scoped>
-.interval_container{
-    display: grid;
+.interval_container {
+  display: grid;
+  align-items: center;
+  justify-items: center;
+
+  >.interval_item {
+    display: flex;
+    width: 90%;
+    justify-content: center;
     align-items: center;
-    justify-items: center;
 
-    >.interval_item{
-        display: flex;
-        width: 90%;
-
-        >img{
-            height: 300px;
-            object-fit: cover;
-            transition: 0.3s ease-in-out;
-            border-radius: 5px;
-            width: 80%;
-        }
+    >img {
+      height: 300px;
+      object-fit: cover;
+      transition: 0.3s ease-in-out;
+      border-radius: 5px;
+      width: 100%;
     }
+  }
 }
-
 </style>
 
 <script setup lang="ts">
@@ -27,46 +28,46 @@ const file_names: any = ref(["/images/example0.jpg", "/images/example1.jpg"]);
 const index: any = ref(0);
 
 const cycle_through = () => {
-    let temporary_index: number = index.value;
-    const array_len: number = file_names.value.length;
-    temporary_index++;
-    if(temporary_index >= array_len){
-        temporary_index = 0; 
-    }
-    index.value = temporary_index;
+  let temporary_index: number = index.value;
+  const array_len: number = file_names.value.length;
+  temporary_index++;
+  if (temporary_index >= array_len) {
+    temporary_index = 0;
+  }
+  index.value = temporary_index;
 }
 
 
-onUpdated(()=>{
-    let image_tag: HTMLElement | null = document.querySelector('.intervalic_image_tag');
-        setTimeout(()=>{
-            if (image_tag !== null){
-                image_tag.style.opacity = '1';
-            }
-        }, 300); 
+onUpdated(() => {
+  let image_tag: HTMLElement | null = document.querySelector('.intervalic_image_tag');
+  setTimeout(() => {
+    if (image_tag !== null) {
+      image_tag.style.opacity = '1';
+    }
+  }, 300);
 });
 
-onMounted(()=>{
-    let image_tag: HTMLElement | null = document.querySelector('.intervalic_image_tag');
+onMounted(() => {
+  let image_tag: HTMLElement | null = document.querySelector('.intervalic_image_tag');
 
-    setInterval(()=>{
-        if(image_tag !== null) {
-            image_tag.style.opacity = '0';
-        }
-        setTimeout(()=>{
-            cycle_through();
-        }, 300);
-    }, 6000)
+  setInterval(() => {
+    if (image_tag !== null) {
+      image_tag.style.opacity = '0';
+    }
+    setTimeout(() => {
+      cycle_through();
+    }, 300);
+  }, 6000)
 });
 
 </script>
 
 <template>
-    <article class="interval_container">
-        <div class="interval_item">
-            <img class="intervalic_image_tag" :src="file_names[index]">
-        </div>
-    </article>
+  <article class="interval_container">
+    <div class="interval_item">
+      <img class="intervalic_image_tag" :src="file_names[index]">
+    </div>
+  </article>
 </template>
 
 
--- src/components/PastriesItems.vue
+++ src/components/PastriesItems.vue
@@ -1,139 +1,151 @@
 <template>
-    <article class="menu_article">
-        <h2 class="pastries_title item_h2_tag">{{ breads[index]['Title'] }}</h2>
-        <p class="pastries_desc item_p_tag">{{ breads[index]['Desc'] }}</p>
-        <span class="control_span">
-            <img @click="index_dwn" src="/svgs/larrow.svg" alt="">
-            <img @click="index_up" src="/svgs/rarrow.svg" alt="">
-        </span>
-    </article>
+  <article class="menu_article">
+    <h2 class="pastries_title item_h2_tag">{{ breads[index]['Title'] }}</h2>
+    <p class="pastries_desc item_p_tag">{{ breads[index]['Desc'] }}</p>
+    <span class="control_span">
+      <img @click="index_dwn" src="/svgs/larrow.svg" alt="">
+      <img @click="index_up" src="/svgs/rarrow.svg" alt="">
+    </span>
+  </article>
 </template>
 
 <script setup lang="ts">
 import { nextTick, onUpdated, ref } from 'vue';
 const index = ref(0);
 const breads = [
-    {
-        "Title": "Sourdough",
-        "Desc": "Savor the tangy perfection of our artisanal Sourdough Delight. Crafted through natural fermentation, bread offers a crispy crust and chewy texture. Versatile and flavorful, it's perfect with butter or paired with artisanal cheeses.",
-        "Img": "/images/pexels-marta-dzedyshko-7693924.jpg"
-    },
-    {
-        "Title": "White bread",
-        "Desc": "Indulge in simplicity with our White Bread. Soft, fluffy, and versatile. Perfect for toasting or crafting your favorite sandwiches. Embrace the classic goodness with every bite.",
-        "Img": "/images/whitebread.jpg"
-    },
-    {
-        "Title": "Whole grain bread",
-        "Desc": "Savor the wholesome goodness of our Nutty Whole Grain Bread. Packed with fiber and nutrients, it's a hearty choice for a nutritious start or a satisfying sandwich foundation.",
-        "Img": "/images/pexels-marta-dzedyshko-7693939.jpg"
-    },
-    {
-        "Title": "Rye bread",
-        "Desc": "Indulge in the rich amalgamation of wholesome grains found in our Rye Bread. Its nutty, tangy profile adds to its versatility, making it an ideal canvas for various spreads or a distinctive choice for your sandwich creation.",
-        "Img": "/images/pexels-nadin-sh-11214698.jpg"
-    }
+  {
+    "Title": "Sourdough",
+    "Desc": "Savor the tangy perfection of our artisanal Sourdough Delight. Crafted through natural fermentation, bread offers a crispy crust and chewy texture. Versatile and flavorful, it's perfect with butter or paired with artisanal cheeses.",
+    "Img": "/images/pexels-marta-dzedyshko-7693924.jpg"
+  },
+  {
+    "Title": "White bread",
+    "Desc": "Indulge in simplicity with our White Bread. Soft, fluffy, and versatile. Perfect for toasting or crafting your favorite sandwiches. Embrace the classic goodness with every bite.",
+    "Img": "/images/whitebread.jpg"
+  },
+  {
+    "Title": "Whole grain bread",
+    "Desc": "Savor the wholesome goodness of our Nutty Whole Grain Bread. Packed with fiber and nutrients, it's a hearty choice for a nutritious start or a satisfying sandwich foundation.",
+    "Img": "/images/pexels-marta-dzedyshko-7693939.jpg"
+  },
+  {
+    "Title": "Rye bread",
+    "Desc": "Indulge in the rich amalgamation of wholesome grains found in our Rye Bread. Its nutty, tangy profile adds to its versatility, making it an ideal canvas for various spreads or a distinctive choice for your sandwich creation.",
+    "Img": "/images/pexels-nadin-sh-11214698.jpg"
+  }
 ]
 
-const index_up = (event: any) =>{
-    
-    let bread_title : HTMLElement | null = document.querySelector('.pastries_title');
-    let bread_desc : HTMLElement | null = document.querySelector('.pastries_desc');
-    
-    if(bread_title !== null && bread_desc !== null){
-        remove_visibility(bread_title, bread_desc)
-    } else {
-        return
+const index_up = (event: any) => {
+
+  let bread_title: HTMLElement | null = document.querySelector('.pastries_title');
+  let bread_desc: HTMLElement | null = document.querySelector('.pastries_desc');
+
+  if (bread_title !== null && bread_desc !== null) {
+    remove_visibility(bread_title, bread_desc)
+    on_click_effect(event.target);
+  } else {
+    return
+  }
+
+  setTimeout(() => {
+    if (event.type === "click") {
+      let index_val = index.value;
+      index_val++;
+      if (index_val >= breads.length) {
+        index_val = 0;
+      }
+      index.value = index_val
     }
 
-    setTimeout(()=>{   
-        if(event.type === "click"){
-            let index_val = index.value;
-            index_val++;
-            if(index_val >= breads.length){
-                index_val = 0;
-            }
-            index.value = index_val
-        }
-
-    }, 300);
+  }, 300);
 }
 
-const index_dwn = (event: any) =>{
-    let bread_title : HTMLElement | null = document.querySelector('.pastries_title');
-    let bread_desc : HTMLElement | null = document.querySelector('.pastries_desc');
-    
-    if(bread_title !== null && bread_desc !== null){
-        remove_visibility(bread_title, bread_desc)
-    } else {
-        return
-    }
+const on_click_effect = (event: any) => {
+  event.style['transition'] = '0.3s ease-in-out'
+  event.style['width'] = '44px';
+  setTimeout(() => {
+    event.style['width'] = '';
+  }, 300)
+}
 
-    setTimeout(()=>{   
-        if(event.type === "click"){
-            let index_val = index.value;
-            index_val--;
-            if(index_val < 0){
-                index_val = breads.length - 1;
-            }
-            index.value = index_val
-        }
-    }, 300);
+
+const index_dwn = (event: any) => {
+  let bread_title: HTMLElement | null = document.querySelector('.pastries_title');
+  let bread_desc: HTMLElement | null = document.querySelector('.pastries_desc');
+
+  if (bread_title !== null && bread_desc !== null) {
+    remove_visibility(bread_title, bread_desc)
+    on_click_effect(event.target);
+  } else {
+    return
+  }
+
+  setTimeout(() => {
+    if (event.type === "click") {
+      let index_val = index.value;
+      index_val--;
+      if (index_val < 0) {
+        index_val = breads.length - 1;
+      }
+      index.value = index_val
+    }
+  }, 300);
 }
 
 const remove_visibility = (title: HTMLElement, desc: HTMLElement) => {
-    title.style.opacity='0';
-    desc.style.opacity='0';
+  title.style.opacity = '0';
+  desc.style.opacity = '0';
 }
 
-onUpdated(()=>{
-    let bread_title : HTMLElement | null = document.querySelector('.pastries_title');
-    let bread_desc : HTMLElement | null = document.querySelector('.pastries_desc');
+onUpdated(() => {
+  let bread_title: HTMLElement | null = document.querySelector('.pastries_title');
+  let bread_desc: HTMLElement | null = document.querySelector('.pastries_desc');
 
-    nextTick(()=>{
-        if(bread_title !== null){
-            bread_title.style.opacity = '1';
-        }
+  nextTick(() => {
+    if (bread_title !== null) {
+      bread_title.style.opacity = '1';
+    }
 
-        setTimeout(()=>{
-            if(bread_desc !== null){
-                bread_desc.style.opacity = '1';
-            }
-        }, 150)
-    })
-           
+    setTimeout(() => {
+      if (bread_desc !== null) {
+        bread_desc.style.opacity = '1';
+      }
+    }, 150)
+  })
+
 })
 
 </script>
 
 <style lang="scss" scoped>
 .menu_article {
-    display: grid;
-    justify-items: start;
+  display: grid;
+  justify-items: start;
+  justify-self: center;
+  align-items: center;
+  width: 90%;
+  text-align: start;
+  row-gap: 35px;
+
+
+  >.pastries_title {
+    transition: 0.3s ease-in-out;
+  }
+
+  >.pastries_desc {
+    transition: 0.3s ease-in-out;
+  }
+
+  >.control_span {
+    display: flex;
+    flex-wrap: wrap;
+    width: 85%;
+    justify-content: space-between;
     justify-self: center;
-    align-items: center;
-    width: 90%;
-    text-align: start;
-    row-gap: 35px;
 
-
-    >.pastries_title{
-        transition: 0.3s ease-in-out;
+    >img {
+      width: 40px;
     }
- 
-    >.pastries_desc{
-        transition: 0.3s ease-in-out;
-    }
-
-    >.control_span{
-        display: flex;
-        flex-wrap: wrap;
-        width: 85%;
-        justify-content: space-between;
-        justify-self: center;
-        >img{
-            width: 40px;
-        }
-    }
+  }
 }
 </style>
